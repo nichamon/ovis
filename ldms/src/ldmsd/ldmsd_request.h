a6cde45740d59311b7780c5b60a07c7bf5eb30e7
nichamon <nichamon@ogc.us>
2024-03-06 10:24:09 -0600

    Revert "Add the banner configuration command"
    This reverts commit 2f1aa48a5df36f5b82d9b74d8e22d4c502a1f12d.

--- ldms/src/ldmsd/ldmsd_request.h
+++ ldms/src/ldmsd/ldmsd_request.h
@@ -154,7 +154,6 @@ enum ldmsd_request {
 	LDMSD_WORKER_THR_SET_REQ,
 	LDMSD_DEFAULT_CREDITS_REQ,
 	LDMSD_PID_FILE_REQ,
-	LDMSD_BANNER_MODE_REQ,
 
 	/* failover requests by user */
 	LDMSD_FAILOVER_CONFIG_REQ = 0x700, /* "failover_config" user command */
